f4a8e36b7b5643aed97d61431ef4c536c70218c1
rodrigorgrb@gmail.com <rodrigorgrb@gmail.com>
2024-09-30 19:13:46 -0300

    feat: add sacola page

--- app/checkout/sacola/page.js
+++ app/checkout/sacola/page.js
@@ -1,11 +1,63 @@
-import Button from "../../components/Button";
+import style from "./Style.module.css";
 
 export default function Page() {
+  const products = [
+    {
+      name: "iPhone 12 Mini",
+      image:
+        "https://cdn.awsli.com.br/2500x2500/1919/1919257/produto/239100238/tit-nio-natural--1--n14c8ix73w.jpg",
+      price: {
+        value: 1590.0,
+        old: 1600.0,
+      },
+    },
+    {
+      name: "iPhone 15 Mini",
+      image:
+        "https://cdn.awsli.com.br/2500x2500/1919/1919257/produto/239100238/tit-nio-natural--1--n14c8ix73w.jpg",
+      price: {
+        value: 1590.0,
+      },
+    },
+    {
+      name: "iPhone 11 Mini",
+      image:
+        "https://cdn.awsli.com.br/2500x2500/1919/1919257/produto/239100238/tit-nio-natural--1--n14c8ix73w.jpg",
+      price: {
+        value: 1590.0,
+      },
+    },
+  ];
   return (
     <>
-      <h1>Pagina de sacola</h1>
-      <Button variant="primary"> Finalizar agora </Button>
-      <Button variant="secondary"> Voltar ao inicio do protótipo </Button>
+      <div className={style.container}>
+        <div className={style.list}>
+          {products.map((product) => {
+            return (
+              <>
+                <div className={style.item}>
+                  <div className={style.imageWrapper}>
+                    <img
+                      className={style.image}
+                      src={product.image}
+                      alt="iPhone 12 Mini"
+                    />
+                  </div>
+                  <div className={style.nameProduct}>{product.name}</div>
+                  <div className={style.price}>
+                    <div className={style.oldPriceValue}>
+                      {product.price.old}
+                    </div>
+                    <div className={style.priceValue}>
+                      {product.price.value}
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
